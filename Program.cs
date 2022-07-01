f0c7043d4bba1fb2ec30e6149fbd3f119e710dba
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-02 02:44:05 +0300

    Task #1 is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #2
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max;
+if(a > b) max = a;
+else max = b;
+Console.Write("Большее число равно ");
+Console.WriteLine(max);
