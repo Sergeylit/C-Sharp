4c337f748327f72dcb0ed228b1b5f7dc6fa8be50
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-02 22:31:25 +0300

    minmax 3 numbers

---
+++ minmax3.cs
@@ -0,0 +1,14 @@
+﻿// #4
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+if(a > b) max = a;
+else max = b;
+if(c > max) max = c;
+Console.Write("Наибольшее число равно ");
+Console.WriteLine(max);
