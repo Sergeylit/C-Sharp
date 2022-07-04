65c6069268888b66941869ef94b6e2be9e7e877e
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-04 17:29:08 +0300

    Delete even.cs

--- even.cs
+++
@@ -1,8 +0,0 @@
-﻿// #6
-
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-if(number % 2 == 0)
-    Console.Write("Число {0} четное", number);
-else
-    Console.Write("Число {0} нечетное", number);
