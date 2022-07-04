19b845d141a8a20f49cdf23b7f710c88df490072
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-04 17:27:08 +0300

    Weekday

---
+++ dayOfTheWeek.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите номер дня недели");
+weekDay:
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+switch(dayNumber)
+{
+    case 1:
+    Console.WriteLine("Не выходной");
+    break;
+
+    case 2:
+    Console.WriteLine("Не выходной");
+    break;
+    
+    case 3:
+    Console.WriteLine("Не выходной");
+    break;
+
+    case 4:
+    Console.WriteLine("Не выходной");
+    break;
+
+    case 5:
+    Console.WriteLine("Не выходной");
+    break;
+
+    case 6:
+    Console.WriteLine("Выходной!");
+    break;
+
+    case 7:
+    Console.WriteLine("Выходной!");
+    break;
+
+    default:
+    Console.WriteLine("Введено некорректное число. Введите другое число");
+    goto weekDay;
+}
