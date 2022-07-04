6654a8950588fe37a4eed636fe5d1033c367d465
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-04 17:14:03 +0300

    Трехзначное

--- rank3number2.cs
+++ rank3number2.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите трёхзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if(number / 100 < 1 || number / 1000 >= 1)
+    {
+        Console.WriteLine("Число не трёхзначное");
+    }
+    else
+    {
+        int thirdNumber = (number % 10);
+        Console.WriteLine(thirdNumber);
+    }
