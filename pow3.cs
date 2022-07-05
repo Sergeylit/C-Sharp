57b1ecd8b9d35185f9d6dce752676fd1ba60c9db
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-06 01:03:50 +0300

    Power of 3

---
+++ pow3.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+double number = Convert.ToDouble(Console.ReadLine());
+for(double i = 0; i < number; i++)
+{
+    double pow = Math.Pow(i, 3);
+    Console.WriteLine(pow);
+}
