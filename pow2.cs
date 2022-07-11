ded803bb55e84652ba36b6e221a80d30d3f3aac3
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-11 16:43:36 +0300

    Число A в степени B

---
+++ pow2.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число, возводимое в степень");
+double number = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите степень числа");
+double power = Convert.ToDouble(Console.ReadLine());
+double result = Math.Pow(number, power);
+Console.WriteLine("Результат равен " + result);
