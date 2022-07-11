177463f58aa67595ed87d3507b7c49c0659ef918
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-11 19:37:52 +0300

    Сумма цифр числа

---
+++ array.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое число");
+int sum = 0;
+int number = Convert.ToInt32(Console.ReadLine());
+while(number != 0)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+Console.WriteLine("Сумма цифр числа равна " + sum);
