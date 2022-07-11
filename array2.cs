f12d8428b0700ebe42d39bda5dc8c94501c7f150
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-11 18:57:29 +0300

    Массив из 8 элементов с выводом на экран

---
+++ array2.cs
@@ -0,0 +1,6 @@
+﻿int[] newArray = new int[8];
+for(int i = 0; i < newArray.Length; i++)
+{
+    newArray[i] = new Random().Next(1, 1000);
+    Console.WriteLine("Элемент массива {0} - {1}", i+1, newArray[i]);
+}
