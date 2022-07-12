2059e425575ad138acda9042fa059d4b81234fb6
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-13 02:34:34 +0300

    Разница между минимальным и максимальным элементами массива

---
+++ minMaxDifference.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = new int[20];
+int min = 100, max = 0;
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(1, 100);
+    if(numbers[i] < min && numbers[i] != min) min = numbers[i];
+    else max = numbers[i];
+    Console.WriteLine("Элемент номер {0} равен {1}", i, numbers[i]);
+}
+Console.WriteLine();
+Console.WriteLine("Минимальное {0}, максимальное {1}", min, max);
+int difference = max - min;
+Console.Write("Разница между наименьшим и наибольшим значениями массива равна " + difference);
