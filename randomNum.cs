2514cba7e50b5a747aed97b340561c8691948f4f
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-13 02:00:21 +0300

    Массив из случайных чисел, показать количество четных чисел в массиве

---
+++ randomNum.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = new int[20];
+int count = 0;
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(100, 999);
+    if(numbers[i] % 2 == 0) count++;
+    Console.WriteLine("Число номер {0} равно {1}", i+1, numbers[i]);
+}
+Console.WriteLine("Количество четных чисел в массиве - " + count);
