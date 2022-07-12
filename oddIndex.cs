d92f4f53b2fdcd43b16d3916bb5a327f40526228
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-13 02:10:45 +0300

    Сумма элементов массива с нечетным индексом

---
+++ oddIndex.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = new int[20];
+int sum = 0;
+for(int i = 1; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(-1000, 1000);
+    sum = sum + numbers[i];
+    Console.WriteLine("Элемент номер {0} равен {1}", i, numbers[i]);
+    i++;
+}
+Console.WriteLine("Сумма элементов массива с нечетным индексом равна " + sum);
