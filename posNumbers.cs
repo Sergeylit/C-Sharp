582e516596087ac356cc0d561d427d0efcf6bc74
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-18 16:42:51 +0300

    Ввод чисел, поиск количества положительных чисел

---
+++ posNumbers.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите количество цифр: ");
+int numbersIndex = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите несколько чисел, после ввода числа нажимайте enter");
+int[] numbers = new int [numbersIndex];
+for(int i = 0; i < numbers.Length; i++) numbers[i] = Convert.ToInt32(Console.ReadLine());
+    int count = 0;
+for(int i = 0; i < numbers.Length; i++)
+if(numbers[i] > 0)
+    count++;
+Console.WriteLine("Чисел больше нуля: " + count);
