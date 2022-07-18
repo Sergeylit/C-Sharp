2994f8a20e3f76d586e9683b1249c81c96183357
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-18 17:32:06 +0300

    Формула нахождения точки пересечения двух прямых на плоскости

--- twoPoints.cs
+++ twoPoints.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double  b1 = 9, b2 = -5, 
+        k1 = -2, k2 = 4;
+
+double x = (b1 - b2) / (k2 - k1);
+double y = x * k1 + b1;
+Console.WriteLine("({0}; {1})", Math.Round(k1 * x + b1, 3), Math.Round(k2 * x + b2, 3));
