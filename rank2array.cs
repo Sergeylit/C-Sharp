f6968628569ff3d913be6bb47b49940dc41dacce
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-21 05:04:23 +0300

    Двумерный массив со случайными числами

---
+++ rank2array.cs
@@ -0,0 +1,18 @@
+﻿int[,] array = new int[10, 10];
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+        Console.WriteLine();
+}
+
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0} ", array[i, j]);
+        }
+        Console.WriteLine();
+}
