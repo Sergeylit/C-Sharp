0cc30af1017664ec0583f2fd067aecf3fa0ce455
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-31 23:20:47 +0300

    Вывод натуральных чисел от M до N

---
+++ task64.cs
@@ -0,0 +1,6 @@
+﻿int M = new Random().Next(0, 51);
+int N = new Random().Next(51, 100);
+Console.WriteLine("start {0}, finish {1}", M, N);
+Console.WriteLine();
+for(int i = M + 1; i < N - 1; i++)
+    Console.WriteLine(i);
