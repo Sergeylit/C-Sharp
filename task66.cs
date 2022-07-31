1d4d15b0634156f8d6fdb9194ecb977a97ab2b5c
SergeyLitovchenko <sergeylit98@gmail.com>
2022-07-31 23:27:00 +0300

    Сумма чисел между M и N

---
+++ task66.cs
@@ -0,0 +1,8 @@
+﻿int M = new Random().Next(0, 51);
+int N = new Random().Next(51, 100);
+int sum = 0;
+Console.WriteLine("start {0}, finish {1}", M, N);
+Console.WriteLine();
+for(int i = M + 1; i <= N - 1; i++)
+    sum += i;
+Console.WriteLine("Сумма элементов от M до N равна " + sum);
