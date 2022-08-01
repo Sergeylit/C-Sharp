2b53e7dd6d3255d4182bf98cb883e5ff1b688c3d
SergeyLitovchenko <sergeylit98@gmail.com>
2022-08-01 04:07:57 +0300

    Вычисление значения функции Аккермана

---
+++ task68.cs
@@ -0,0 +1,11 @@
+﻿static int Function(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (n != 0 && m == 0)
+        return Function(n - 1, 1);
+    else
+        return Function(n - 1, Function(n, m - 1));
+}
+
+Console.WriteLine(Function(3, 2));
